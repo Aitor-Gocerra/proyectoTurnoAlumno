78b6d83fc94bf0b622ac73c44adc05668ea275ce
Aitor Gomez Cerrato <aitorgomezcerrato@gmail.com>
2025-12-05 12:40:30 +0100

    Update configDB.php

--- src/app/config/configDB.php
+++ src/app/config/configDB.php
@@ -2,5 +2,5 @@
     DEFINE("servidor", "21.daw.esvirgua.com");
     DEFINE("usuario", "daw_userbd21");
     DEFINE("password", "21NTiJq]K)7]SQB=");
-    DEFINE("nombreDB", "daw_21_BD1");
+    DEFINE("nombreDB", "daw_21_BD2");
 ?>
